6348c372b338345e88b57d8744dd7323b8e59a7b
Sneezycheesy <j.niksnl@outlook.com>
2022-02-28 10:09:59 +0100

    Added code for tutorial 7, 'analog writes'

---
+++ arduino_tutorial_7/arduino_tutorial_7.ino
@@ -0,0 +1,22 @@
+const int pin = 3;
+const int brightOff = 0;
+const int broghtLow = 5;
+const int brightMed = 130;
+const int brightHigh = 255;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(pin, brightLow);
+  delay(5000);
+  analogWrite(pin, brightMed);
+  delay(5000);
+  analogWrite(pin, brightHigh);
+  delay(5000);
+  analogWrite(pin, brightOff);
+  delay(5000);
+}
