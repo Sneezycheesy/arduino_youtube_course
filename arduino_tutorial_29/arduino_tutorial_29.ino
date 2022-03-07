c9874ce4f624a8b04ed34536443923a60b84723d
Sneezycheesy <j.niksnl@outlook.com>
2022-03-07 11:12:24 +0100

    Added buzzer for too high and low values

--- arduino_tutorial_29/arduino_tutorial_29.ino
+++ arduino_tutorial_29/arduino_tutorial_29.ino
@@ -7,6 +7,7 @@ const int boadRate = 9600;
 
 const int btnIncreasePin = 8;
 const int btnDecreasePin = 12;
+const int buzzPin = 6;
 const int ledPin = 3;
 int ledState = 0; // Always start led off
 int btnIncreasePrevState = 1; 
@@ -20,21 +21,39 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(btnIncreasePin, INPUT);
   pinMode(btnDecreasePin, INPUT);
+  pinMode(buzzPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   btnIncreaseCurrentState = digitalRead(btnIncreasePin);
   btnDecreaseCurrentState = digitalRead(btnDecreasePin);
-  if (btnIncreaseCurrentState == 0 && ledState < 255) {
-    ledState++;
+  if (btnIncreaseCurrentState == 0 && ledState <= 256) {
+      ledState++;
   }
-  if (btnDecreaseCurrentState == 0 && ledState > 0) {
-    ledState--;
+  if (btnDecreaseCurrentState == 0 && ledState >= -1) {
+      ledState--;
   }
+
+  if (ledState > 255) {
+    ledState = 255;
+    buzz();
+  }
+
+  if (ledState < 0) {
+    ledState = 0;
+    buzz();
+  }
+  
   analogWrite(ledPin, ledState);
   Serial.print("Decreasing pin: ");
   Serial.print(btnDecreaseCurrentState);
   Serial.print(", Increasing pin: ");
   Serial.println(btnIncreaseCurrentState);
+}
+
+void buzz() {
+  digitalWrite(buzzPin, HIGH);
+  delay(dt);
+  digitalWrite(buzzPin, LOW);
 }
