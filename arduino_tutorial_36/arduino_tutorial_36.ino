dc0967e1273c77cd70eaab99b8a8d3a5418f0af0
Sneezycheesy <j.niksnl@outlook.com>
2022-03-08 15:17:10 +0100

    Added code for tutorial 36 + some fun extra stuff

---
+++ arduino_tutorial_36/arduino_tutorial_36.ino
@@ -0,0 +1,47 @@
+/*
+ * Tutorial 36 Understanding tilt switches
+ * https://youtu.be/SYDuRyweeLA
+ */
+
+const int baudRate = 9600;
+const int dt = 200; // delaytime
+const int tiltPin = 12;
+const int redLedPin = 2;
+const int greenLedPin = 3;
+const int buzzPin = 13;
+int tiltValue;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(baudRate);
+  pinMode(tiltPin, INPUT);
+  pinMode(buzzPin, OUTPUT);
+  digitalWrite(tiltPin, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tiltValue = digitalRead(tiltPin);
+  Serial.println(tiltValue);
+  if (tiltValue != 0) {
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(greenLedPin, LOW);
+
+    buzzWarning(3);
+  } else {
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(greenLedPin, HIGH);
+  }
+  delay(dt);
+}
+
+void buzzWarning(int amount) {
+  int j = 0;
+  while (j < amount) {
+    digitalWrite(buzzPin, HIGH);
+    delay(dt);
+    digitalWrite(buzzPin, LOW);
+    delay(dt);
+    j++;
+  }
+}
