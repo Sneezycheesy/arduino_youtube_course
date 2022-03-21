e3d1ae90148a44b02ce34dc22ce05b0ed1a0f263
Sneezycheesy <j.niksnl@outlook.com>
2022-03-21 11:26:48 +0100

    Added command codes and updated API calls

--- arduino_tutorial_64/arduino_tutorial_64.ino
+++ arduino_tutorial_64/arduino_tutorial_64.ino
@@ -3,8 +3,30 @@
 const int baudRate = 9600;
 const int dt = 500; // delaytime
 const byte controlPin = 11;
-int irCommand;
-IRrecv IR;
+uint32_t irCommand;
+
+const unsigned int cmdZero = 0xE916FF00;
+const unsigned int cmdOne = 0xF30CFF00;
+const unsigned int cmdTwo = 0xE718FF00;
+const unsigned int cmdThree = 0xA15EFF00;
+const unsigned int cmdFour = 0xF708FF00;
+const unsigned int cmdFive = 0xE31CFF00;
+const unsigned int cmdSix = 0xA55AFF00;
+const unsigned int cmdSeven = 0xBD42FF00;
+const unsigned int cmdEight = 0xAD52FF00;
+const unsigned int cmdNine = 0xB54AFF00;
+const unsigned int cmdPower = 0xBA45FF00;
+const unsigned int cmdVolUp = 0xB946FF00;
+const unsigned int cmdVolDown = 0xEA15FF00;
+const unsigned int cmdFuncStop = 0xB847FF00;
+const unsigned int cmdBack = 0xBB44FF00;
+const unsigned int cmdPause = 0xBF40FF00;
+const unsigned int cmdNext = 0xBC43FF00;
+const unsigned int cmdDown = 0xF807FF00;
+const unsigned int cmdUp = 0xF609FF00;
+const unsigned int cmdEQ = 0xE619FF00;
+const unsigned int cmdStopRepeat = 0xF20DFF00;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,11 +36,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (IR.decode() == 0) {
-    
+  if (IrReceiver.decode()) {
+    irCommand = IrReceiver.decodedIRData.decodedRawData;
+    Serial.println(irCommand, HEX);
+    delay(dt);
+    IrReceiver.resume();
   }
-  irCommand = IR.decodedIRData.decodedRawData;
-  Serial.println(irCommand, HEX);
-  delay(dt);
-  IR.resume();
 }
